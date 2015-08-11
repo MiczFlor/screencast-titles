1674b75b100b9bf0fa23c496a1409777cc0e50fb
Micz Flor <micz.flor@web.de>
2015-08-11 14:43:15 +0200

    Create lang-de.php

---
+++ lang-de.php
@@ -0,0 +1,25 @@
+<!-- lang-de.php -->
+<?php
+$lang['pagetitle'] = "Screencast Titel Generator";
+$lang['errorform'] = "Mache es deinem Publikum einfacher und gib die relevante Info an.";
+$lang['langselect'] = "Sprache auswählen";
+$lang['title'] = "Titel";
+$lang['description'] = "Beschreibung";
+$lang['project'] = "Project";
+$lang['version'] = "Version";
+$lang['imgurl'] = "Dein Bild (URL)";
+$lang['target'] = "Zielgruppe";
+$lang['editors'] = "Redaktionelle Arbeit";
+$lang['template'] = "Template-Entwicklung";
+$lang['administration'] = "Administration";
+$lang['trainer'] = "Training";
+$lang['screencaster'] = "Screencaster";
+$lang['name'] = "Name";
+$lang['email'] = "E-mail";
+$lang['language'] = "Sprache";
+$lang['date'] = "Datum";
+$lang['sharethis'] = "Teile diesen Screencast mir der Sourcefabric-Community!";
+$lang['feature'] = "Promote den Screencast auf unserer Seite";
+$lang['submit'] = "Titel erstellen";
+$lang['reset'] = "Zur&uuml;cksetzen";
+?>
