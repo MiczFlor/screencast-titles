216e814f8e4787ad6ae8f2b099200994ad4ff7df
Micz Flor <micz.flor@web.de>
2015-08-11 14:44:04 +0200

    Create lang-pl.php

---
+++ lang-pl.php
@@ -0,0 +1,25 @@
+<!-- lang-pl.php -->
+<?php
+$lang['pagetitle'] = "Utwórz swój własny screencast";
+$lang['errorform'] = "Pomóż potecjalnym widzom i podaj dodatkowe informacje.";
+$lang['langselect'] = "wybierz język";
+$lang['title'] = "Tytuł";
+$lang['description'] = "Opis";
+$lang['project'] = "Project";
+$lang['version'] = "Wersja";
+$lang['imgurl'] = "Your image (URL)";
+$lang['target'] = "Grupa docelowa";
+$lang['editors'] = "Redaktorzy i dziennikarze";
+$lang['template'] = "Graficy i designerzy";
+$lang['administration'] = "Administracja";
+$lang['administration'] = "Administration";
+$lang['trainer'] = "Trainer";
+$lang['name'] = "Twoje imię";
+$lang['email'] = "Twój adres email";
+$lang['language'] = "Język";
+$lang['date'] = "Data";
+$lang['sharethis'] = "Podziel się tym screencastem ze społecznością Sourcefabric?";
+$lang['feature'] = "Umieść screencast na swojej stronie";
+$lang['submit'] = "Generuj screen";
+$lang['reset'] = "Wyczyść formularz";
+?>
