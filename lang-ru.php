bfa8707a8b97d1490b14d1452e8206de9d3dde43
Micz Flor <micz.flor@web.de>
2015-08-11 14:44:18 +0200

    Create lang-ru.php

---
+++ lang-ru.php
@@ -0,0 +1,25 @@
+<!-- lang-ru.php -->
+<?php
+$lang['pagetitle'] = "Создай свой собственный ролик";
+$lang['errorform'] = "Облегчи жизнь своим пользователям и добавь всю необходимую информацию";
+$lang['langselect'] = "Выбрать язык";
+$lang['title'] = "Заголовок";
+$lang['description'] = "Описание";
+$lang['project'] = "Project";
+$lang['version'] = "Версия";
+$lang['imgurl'] = "Your image (URL)";
+$lang['target'] = "Целевая аудитория";
+$lang['editors'] = "Редакторы &amp; Журналисты";
+$lang['template'] = "Разработчики шаблонов";
+$lang['administration'] = "Управленческий персонал";
+$lang['administration'] = "Administration";
+$lang['trainer'] = "Trainer";
+$lang['name'] = "Ваше имя";
+$lang['email'] = "E-mail";
+$lang['language'] = "Язык";
+$lang['date'] = "Дата создания";
+$lang['sharethis'] = "Поделись своим роликом с сообществом Sourcefabric!";
+$lang['feature'] = "Размести ролик на нашем сайте"; 
+$lang['submit'] = "Генерировать ролик";
+$lang['reset'] = "Очистить форму";
+?>
