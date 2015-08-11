e7578804694ca3d92bf3cfa76ef8f4c6164e631e
Micz Flor <micz.flor@web.de>
2015-08-11 14:42:53 +0200

    Create lang-cz.php

---
+++ lang-cz.php
@@ -0,0 +1,25 @@
+<!-- lang-cz.php -->
+<?php
+$lang['pagetitle'] = "Vytvořte svůj vlastní screencast";
+$lang['errorform'] = "Zjednodušte to pro své posluchače a poskytněte všechny relevantní informace.";
+$lang['langselect'] = "Vyberte jazyk";
+$lang['title'] = "Název";
+$lang['description'] = "Popis";
+$lang['project'] = "Project";
+$lang['version'] = "Verze";
+$lang['imgurl'] = "Your image (URL)";
+$lang['target'] = "Cílova skupina";
+$lang['editors'] = "Editoři &amp; Novináři";
+$lang['template'] = "Vývoj šablon";
+$lang['administration'] = "Administrace";
+$lang['administration'] = "Administration";
+$lang['trainer'] = "Trainer";
+$lang['name'] = "Vaše jméno";
+$lang['email'] = "Vaše e-mailová adresa";
+$lang['language'] = "Jazyk";
+$lang['date'] = "Datum";
+$lang['sharethis'] = "Sdílejte tento screencast s komunitou Sourcefabric!";
+$lang['feature'] = "Zobrazte svůj screencast na naší webové stránce";
+$lang['submit'] = "Vygenerovat screen";
+$lang['reset'] = "Prázdný dokument";
+?>
